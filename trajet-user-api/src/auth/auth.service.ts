9ec575cea684f679c6a0c8d080b7eb0b68d14245
dvoumagagnon <70532250+dvoumagagnon@users.noreply.github.com>
2022-12-05 17:18:45 -0500

    Ajout du AuthGuard

--- trajet-user-api/src/auth/auth.service.ts
+++ trajet-user-api/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable } from '@nestjs/common';
 import { CreateUtilisateurDto } from 'src/utilisateurs/dto/create-utilisateur.dto';
 import { UtilisateursService } from 'src/utilisateurs/utilisateurs.service';
 import * as argon2 from 'argon2';
@@ -91,4 +91,24 @@ export class AuthService {
             refreshToken,
         };
     }
+
+    async refreshTokens(userId: string, refreshToken: string) {
+        const user = await this.utilisateursService.findById(userId);
+        
+        if (!user || !user.refreshToken) {
+            throw new ForbiddenException('Access Denied');
+        } 
+        
+        const refreshTokenMatches = await argon2.verify(user.refreshToken, refreshToken);
+        
+        if (!refreshTokenMatches) {
+            throw new ForbiddenException('Access Denied');
+        } 
+        
+        const tokens = await this.getTokens(user.id, user.email);
+        
+        await this.updateRefreshToken(user.id, tokens.refreshToken);
+        
+        return tokens;
+      }
 }
